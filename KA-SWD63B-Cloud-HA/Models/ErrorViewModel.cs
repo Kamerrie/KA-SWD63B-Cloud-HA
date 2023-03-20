81f2e6fb0c5407f75e9d82724bcbf86274d14dce
Katherine Attard <kate.attard.2001@gmail.com>
2023-03-20 11:15:14 +0100

    Add project files.

---
+++ KA-SWD63B-Cloud-HA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KA_SWD63B_Cloud_HA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
